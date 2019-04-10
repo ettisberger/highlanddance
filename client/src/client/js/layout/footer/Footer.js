e9dfe110ec53fbbbf46cc1f33d1f4addba6ca8cb
Andy Ettisberger <andy@ettisberger.ch>
2019-04-10 22:56:46 +0200

    Added svg footer

--- client/src/client/js/layout/footer/Footer.js
+++ client/src/client/js/layout/footer/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import theme, {accent1Color, brandPrimary, Inlay } from '../../theme';
+import theme, {accent1Color, brandPrimary, backgroundColor, Inlay, whiteColor} from '../../theme';
 import Grid from '@material-ui/core/Grid'
 import Mailto from 'react-protected-mailto'
 
@@ -28,11 +28,39 @@ const FooterItem = styled(Grid)`
 `;
 
 const FooterTitle = styled.div`
-  font-weight: bold;
+    font-weight: bold;
 `;
 
 const MailToEncrypted = styled(Mailto)`
-  color: white;
+    color: white;
+`;
+
+const FooterBackground = styled.div`
+    position: relative;
+    width: 100%;
+    height: auto;
+    background-color: ${whiteColor};
+
+    @keyframes swell {
+        0%, 100% {
+        	transform: translate3d(0,0,0);
+        }
+        50% {
+        	transform: translate3d(0,-1px,0);
+        }
+    }
+    
+    svg {
+    	position: relative;
+		width: 100%;
+		height: 100%;
+    }
+
+		
+		#highland-moving {
+			animation: swell 7s ease -1.25s infinite;
+		}
+    }
 `;
 
 class Footer extends Component {
@@ -40,30 +68,48 @@ class Footer extends Component {
     render() {
         return (
             <footer>
-                <FooterWrapper>
-                    <Inlay>
-                        <Grid container justify={'center'}>
-                            <FooterItem container item xs={12} sm={8} direction={'column'}>
-                                <FooterTitle>Kompass</FooterTitle>
-                                <p>
-                                    Highland Dancing Basel<br/>
-                                    www.highlanddance.ch<br/>
-                                    2018/2019
-                                </p>
-                            </FooterItem>
-                            <FooterItem container item xs={12} sm={4} direction={'column'}>
-                                <FooterTitle>Kontakt</FooterTitle>
-                                <p>
-                                   <MailToEncrypted
-                                    email='info@highlanddance.ch'
-                                    headers={
-                                        {subject:'Kontaktaufnahme'}
-                                    }/>
-                                </p>
-                            </FooterItem>
-                        </Grid>
-                    </Inlay>
-                </FooterWrapper>
+                <FooterBackground>
+                    <svg viewBox="0 0 100 25">
+                        <defs>
+                            <linearGradient id="highland" x1="0%" y1="100%" x2="100%" y2="0%" >
+                                <stop offset="0%" stopColor="rgb(37,64,143)" stopOpacity="1" />
+                                <stop offset="45%" stopColor="rgb(37,64,143)" stopOpacity="1" />
+                                <stop offset="100%" stopColor="rgb(52,90,201)" stopOpacity="1" />
+                            </linearGradient>
+                        </defs>
+                        {/*<svg viewBox="0 0 400 100" preserveAspectRatio="none">*/}
+                            <path id="highland-fix" fill="url(#highland)" d="M0 30 V12 Q30 17 55 12 T100 11 V30z"/>
+                        {/*</svg>*/}
+                        {/*<svg viewBox="0 0 400 100" preserveAspectRatio="none">*/}
+                            <path id="highland-moving" fill="url(#highland)" fillOpacity="0.7" d="M0 30 V12 Q40 15 55 12 T100 11 V30z"/>
+                        {/*</svg>*/}
+                    </svg>
+                </FooterBackground>
+
+                {/*<FooterWrapper>*/}
+                {/*    <Inlay>*/}
+                {/*        <Grid container justify={'center'}>*/}
+                {/*            <FooterItem container item xs={12} sm={8} direction={'column'}>*/}
+                {/*                <FooterTitle>Kompass</FooterTitle>*/}
+                {/*                <p>*/}
+                {/*                    Highland Dancing Basel<br/>*/}
+                {/*                    www.highlanddance.ch<br/>*/}
+                {/*                    2018/2019*/}
+                {/*                </p>*/}
+                {/*            </FooterItem>*/}
+                {/*            <FooterItem container item xs={12} sm={4} direction={'column'}>*/}
+                {/*                <FooterTitle>Kontakt</FooterTitle>*/}
+                {/*                <p>*/}
+                {/*                   <MailToEncrypted*/}
+                {/*                    email='info@highlanddance.ch'*/}
+                {/*                    headers={*/}
+                {/*                        {subject:'Kontaktaufnahme'}*/}
+                {/*                    }/>*/}
+                {/*                </p>*/}
+                {/*            </FooterItem>*/}
+                {/*        </Grid>*/}
+                {/*    </Inlay>*/}
+                {/*</FooterWrapper>*/}
             </footer>
         )
     }
--- client/src/client/js/layout/header/PageHeader.js
+++ client/src/client/js/layout/header/PageHeader.js
@@ -3,25 +3,24 @@ import styled from 'styled-components';
 import Hidden from '@material-ui/core/Hidden'
 import {whiteColor, brandPrimary} from '../../theme';
 
-const PageHeaderWrapper = styled.header`
-    height: 100%;
-    width: 100%;
-    background-size: cover;
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-image: url(${props => props.imageUrl});
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
+// const PageHeaderWrapper = styled.header`
+//     height: 100%;
+//     width: 100%;
+//     background-size: cover;
+//     background-position: center center;
+//     background-repeat: no-repeat;
+//     background-image: url(${props => props.imageUrl});
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+// `;
 
-const HeaderTest = styled.header`
+const PagerHeaderWrapper = styled.header`
   position: absolute;
   width: 100%;
   z-index: 1;
   top: 0;
   height: 500px;
-  background-image: linear-gradient(155deg, #000 0%, ${brandPrimary} 50%, ${brandPrimary} 100%); /* fallback */
   background-image: linear-gradient(155deg, #000 0%, ${brandPrimary} 50%, ${brandPrimary} 100%);
   
   &:after {
@@ -30,7 +29,7 @@ const HeaderTest = styled.header`
     background-position: center center;
     background-repeat: no-repeat;
     background-image: url(${props => props.imageUrl});
-    opacity: .02;
+    opacity: .05;
     position: absolute;
     z-index: -1;
     width: 100%;
@@ -38,12 +37,12 @@ const HeaderTest = styled.header`
   }
 `;
 
-const PageHeaderTitle = styled.h1`
-    margin-top: 0;
-    margin-bottom: 0;
-    font-size: 4rem;
-    color: ${whiteColor};
-`;
+// const PageHeaderTitle = styled.h1`
+//     margin-top: 0;
+//     margin-bottom: 0;
+//     font-size: 4rem;
+//     color: ${whiteColor};
+// `;
 
 const Svg = styled.svg`
   position: absolute;
@@ -60,11 +59,11 @@ export default class PageHeader extends Component {
                     {/*<PageHeaderWrapper imageUrl={this.props.imageUrl}>*/}
                     {/*    <PageHeaderTitle className="animated fadeInDown slow">{this.props.title}</PageHeaderTitle>*/}
                     {/*</PageHeaderWrapper>*/}
-                    <HeaderTest imageUrl={this.props.imageUrl}>
+                    <PagerHeaderWrapper imageUrl={this.props.imageUrl}>
                         <Svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270" >
                             <path d="M-314,267 C105,364 400,100 812,279" fill="none" stroke="white" strokeWidth="120" strokeLinecap="round"/>
                         </Svg>
-                    </HeaderTest>
+                    </PagerHeaderWrapper>
                 </Hidden>
             </React.Fragment>
         )
